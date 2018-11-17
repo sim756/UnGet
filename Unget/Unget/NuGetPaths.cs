29657f180d96e10e5ddafbe7ee8416dc73467fd0
Sadequl Islam Mithun <sim756@outlook.com>
2018-11-17 19:40:11 +0600

    added to GitHub

---
+++ Unget/Unget/NuGetPaths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Unget
+{
+    public static class NuGetPaths
+    {
+        public static readonly Dictionary<OSPlatform, string> NuGetCachePaths = new Dictionary<OSPlatform, string>()
+        {
+            {OSPlatform.Windows, Environment.ExpandEnvironmentVariables("%UserProfile%\\.nuget\\packages")},
+            {OSPlatform.Linux, "~/.nuget/packages"},
+            {OSPlatform.OSX, "~/.nuget/packages"}
+        };
+
+        public static string GetNugetCachePathByPlatform()
+        {
+            return (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? NuGetCachePaths[OSPlatform.Windows]
+                : (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                    ? NuGetCachePaths[OSPlatform.Linux]
+                    : ((RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                        ? NuGetCachePaths[OSPlatform.OSX]
+                        : null)));
+        }
+    }
+}
---
+++ Unget/Unget/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Unget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("unget - NuGet Cache Cleaner\n");
+
+                string[] dirs
+                    = Directory.GetDirectories(NuGetPaths.GetNugetCachePathByPlatform(),
+                        "*.*", SearchOption.TopDirectoryOnly);
+
+                int revomalDirCount = 0;
+
+                Parallel.For(0, dirs.Length, i =>
+                {
+                    List<string> cachedDirs
+                        = Directory.GetDirectories(dirs[i], "*.*", SearchOption.TopDirectoryOnly).ToList();
+
+                    if (cachedDirs.Count > 1)
+                    {
+                        Interlocked.Add(ref revomalDirCount, cachedDirs.Count - 1);
+                        cachedDirs.Sort();
+
+                        for (int j = 0; j < cachedDirs.Count - 1; j++)
+                        {
+                            try
+                            {
+                                Directory.Delete(cachedDirs[j], true);
+                                Console.WriteLine($"[DEL] {cachedDirs[j]}");
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine($"\n[ERR] {cachedDirs[j]}\n{exception}\n");
+                            }
+                        }
+                    }
+                });
+
+                Console.WriteLine($"\nTotal {revomalDirCount} folder(s) are deleted from cache.");
+
+                Console.ReadKey(false);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
